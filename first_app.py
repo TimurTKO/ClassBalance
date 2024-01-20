6d49628ed8c76ca21d869fd5b69611bd03a126a0
TimurTKO <151398944+TimurTKO@users.noreply.github.com>
2024-01-20 15:11:32 +0300

    Debug git commit

--- first_app.py
+++ first_app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from pydantic import BaseModel
 #%%
 st.title('My first app')
 #%%
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=['a', 'b', 'c'])
+name = st.text_input("Enter your name", '')
+st.write(f"Hello {name}!")
+x = st.slider("Select an integer x", 0, 10, 1)
+y = st.slider("Select an integer y", 0, 10, 1)
+df = pd.DataFrame({"x": [x], "y": [y] , "x + y": [x + y]}, index = ["addition row"])
+st.write(df)#%%
 
-st.line_chart(chart_data)
 #%%
