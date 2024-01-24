4a578aa31b1e40e453340a067d3c21f88f88c0a6
TimurTKO <151398944+TimurTKO@users.noreply.github.com>
2024-01-24 21:22:46 +0300

    Deleted non-binary cells

--- first_app.py
+++ first_app.py
@@ -3,15 +3,31 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 from pydantic import BaseModel
 #%%
 st.title('My first app')
-#%%
-name = st.text_input("Enter your name", '')
-st.write(f"Hello {name}!")
-x = st.slider("Select an integer x", 0, 10, 1)
-y = st.slider("Select an integer y", 0, 10, 1)
-df = pd.DataFrame({"x": [x], "y": [y] , "x + y": [x + y]}, index = ["addition row"])
-st.write(df)#%%
+#creating and showing histogram in streamlit using seaborn
+#create subplots
+
+df = pd.read_csv('train.csv')
+y = df['income_>50K']
+X = df.drop('income_>50K', axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+df_for_sns = pd.concat([X_train,pd.DataFrame(y_train)], axis=1)
+fig, ax = plt.subplots(figsize=(20, 14))
+
+sns.violinplot(
+    ax=ax,
+    data=df_for_sns, hue="income_>50K", x="workclass", split=True,
+    scale="count", inner="quartile"
+)
+plt.legend(fontsize='x-large')
+ax.tick_params(axis='both', which='major', labelsize='x-large')
+st.pyplot(fig)
+
+
+
+
 
 #%%
